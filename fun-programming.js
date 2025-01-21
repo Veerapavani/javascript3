5900ec9a69cab79dc77bf8f53d8a6b4b09e45f8c
Veerapavani <2211cs010433@mallareddyuniversity.ac.in>
2025-01-21 11:43:17 +0530

    Add files via upload

---
+++ fun-programming.js
@@ -0,0 +1,40 @@
+//function main(a)
+//{
+    //console.log(a);
+  //  console.log(a());
+
+//}
+//main(function(){
+  //  return "iam Veerapavani"
+    
+//})
+//main(function(){
+   // return "iam Veera "
+//})
+
+//function main1(task)
+//{
+  //  console.log(task(10 , 10));
+
+//}
+//main1(function(a,b){
+    //return a+b
+    
+//})
+//main1(function(a,b){
+  //  return a-b
+//})
+//main1(function(a,b){
+  //  return a*b
+    
+//})
+//main1(function(a,b){
+  //  return a/b
+//})
+let a = () => console.log("hello");
+console.log(a);
+a()
+
+let b = () => { return "hello" }
+console.log(b);
+console.log(b())
